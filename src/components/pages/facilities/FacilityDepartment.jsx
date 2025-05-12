fc837b1942a389fdcf74af856f11db88bb7ccd9a
Charmant12 <sgcharmant@compstaffing.com>
2025-05-12 12:51:47 +0200

    refactoring the departments feature

--- src/components/pages/facilities/FacilityDepartment.jsx
+++ src/components/pages/facilities/FacilityDepartment.jsx
@@ -8,6 +8,8 @@ import DateFormatter from '@/components/DateFormatter'
 import '../../../styles/facilities/_facilities.scss'
 import NoteMessage from '@/components/NoteMessage'
 import { ChevronDown } from 'lucide-react';
+import { NotepadText, Layers, Frown, Users } from 'lucide-react';
+import ErrorMessage from '@/components/messages/ErrorMessage'
 
 
 const FacilityDepartmentContent = () => {
@@ -18,6 +20,7 @@ const FacilityDepartmentContent = () => {
     const [complaints, setComplaints] = useState([])
     const [staffCount, setStaffCount] = useState(0)
     const [members, setMembers] = useState([])
+    const [activeTab, setActiveTab] = useState("reports");
 
     // getting the department
     useEffect(() => {
@@ -61,7 +64,7 @@ const FacilityDepartmentContent = () => {
             try {
                 const response = await api.get(`/facilities/departments/${department_id}/members/`)
                 if (response.status === 200) {
-                    // console.log('facility department members', response.data)
+                    console.log('facility department members', response.data)
                     setMembers(response.data.members)
                 }
             } catch (error) {
@@ -150,6 +153,100 @@ const FacilityDepartmentContent = () => {
                     </div>
                 </div>
             </section>
+            <div className="tabs-list">
+                <div
+                    onClick={() => setActiveTab("reports")}
+                    className={`tab ${activeTab === "reports" ? "active" : ""}`}
+                >
+                    <NotepadText size={20} />
+                    Incidents reports
+                </div>
+                <div
+                    onClick={() => setActiveTab("complaints")}
+                    className={`tab ${activeTab === "complaints" ? "active" : ""}`}
+                >
+                    <Frown size={20} /> Complaints
+                </div>
+                {/* <div onClick={() => setActiveTab('documents')} className={`tab ${activeTab === 'documents' ? 'active' : ''}`}>Documents</div> */}
+                <div
+                    onClick={() => setActiveTab("staff")}
+                    className={`tab ${activeTab === "staff" ? "active" : ""}`}
+                >
+                    <Users size={20} /> Staff
+                </div>
+            </div>
+            {/* incident reports */}
+            {activeTab === "reports" && (
+                <div className="report-list-with-notes">
+                    <h2>Incident reports of the department</h2>
+                </div>
+            )}
+            {/* complaints */}
+            {activeTab === "complaints" && (
+                <div>
+                    <div className="table-container">
+                        <table className='review-groups-table'>
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Claim ID</th>
+                                    <th>Patient name</th>
+                                    <th>MRN</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            {complaints && complaints.length > 0 ? (
+                                complaints.map((complaint) => (
+                                    <tbody>
+                                        <tr
+                                            // onClick={() => handleShowComplainDetails(complaint)}
+                                            key={index}
+                                        >
+                                            <td>{index + 1}</td>
+                                            <td>{complaint.id}</td>
+                                            <td>{complaint.patient_name}</td>
+                                            <td>{complaint.medical_record_number}</td>
+                                            <td>
+                                                {<DateFormatter dateString={complaint.created_at} />}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                ))
+                            ) : (
+                                'No complaints available'
+                            )}
+                        </table>
+                    </div>
+                </div>
+            )}
+            {/* staff */}
+            {activeTab === "staff" && (
+                <div>
+                    <div className='table-container'>
+                        <table className='review-groups-table'>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            {Array.isArray(members) && members.length > 0 ? (
+                                members.map((member) => (
+                                    <tbody key={member.id}>
+                                        <tr>
+                                            <td>{member.id}</td>
+                                            <td>{member.first_name} {member.last_name}</td>
+                                            <td>{member.email}</td>
+                                        </tr>
+                                    </tbody>
+                                ))
+                            ) : 'No staff member available'
+                            }
+                        </table>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
