228bcccd3a065d70ea3fa46e080bbdac3cffed1e
Charmant12 <sgcharmant@compstaffing.com>
2025-06-09 16:22:10 +0200

    Added sorting feature on the department

--- src/components/accounts/tabs/Departments.jsx
+++ src/components/accounts/tabs/Departments.jsx
@@ -1,10 +1,17 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { MoveRight, Notebook, PlusCircle, PlusIcon } from "lucide-react";
+import React, { useState, useEffect, useMemo } from "react";
+import {
+  MoveRight,
+  Notebook,
+  PlusCircle,
+  PlusIcon,
+  ListFilter
+} from "lucide-react";
 import api from "@/utils/api";
 import { useRouter } from "next/navigation";
 import AddDepartment from "../forms/department/AddDepartment";
 import "../../../styles/facilities/_facilities.scss";
+import { format } from "date-fns";
 
 const DepartmentsPage = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -14,6 +21,10 @@ const DepartmentsPage = () => {
   const [selectedDepartment, setSelectedDepartment] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
   const [showAddDepartment, setShowAddDepartment] = useState(false);
+  const [sortConfig, setSortConfig] = useState({
+    field: 'name',
+    direction: 'asc',
+  });
   const router = useRouter();
 
   // Fetch all facilities on mount
@@ -23,7 +34,6 @@ const DepartmentsPage = () => {
         setIsLoading(true);
         const response = await api.get(`/facilities/`);
         if (response.status === 200) {
-          // console.log('facilities', response.data)
           setFacilities(response.data);
         }
       } catch (error) {
@@ -49,7 +59,6 @@ const DepartmentsPage = () => {
         });
         if (response.status === 200) {
           setDepartments(response.data.results);
-          console.log(response.data.results);
         }
       } catch (error) {
         setErrorMessage("Error fetching departments");
@@ -62,6 +71,32 @@ const DepartmentsPage = () => {
     fetchDepartments();
   }, [selectedFacilityId]);
 
+  // sorting
+  const sortedDepartments = useMemo(() => {
+    if (!departments) return []
+
+    const sortableDepartments = [...departments]
+    return sortableDepartments.sort((a, b) => {
+      if (sortConfig.field === 'created_at') {
+        const dateA = new Date(a.created_at)
+        const dateB = new Date(b.created_at)
+        return sortConfig.direction === 'asc' ? dateA - dateB : dateB - dateA
+      }
+      const nameA = a.name.toLowerCase()
+      const nameB = b.name.toLowerCase()
+      if (nameA < nameB) return sortConfig.direction === 'asc' ? -1 : 1
+      if (nameA > nameB) return sortConfig.direction === 'asc' ? 1 : -1
+      return 0
+    })
+  }, [departments, sortConfig])
+
+  const handleSortChange = (field) => {
+    setSortConfig(prev => ({
+      field,
+      direction: prev.field === field && prev.direction === 'asc' ? 'desc' : 'asc'
+    }))
+  }
+
   const handleDepartmentClick = (department_id) => {
     router.push(
       `/facilities/${selectedFacilityId}/departments/${department_id}`
@@ -72,6 +107,9 @@ const DepartmentsPage = () => {
     setDepartments(prev => Array.isArray(prev) ? [...prev, newDepartment] : [newDepartment]);
   }
 
+  function formatDate(isoString) {
+    return format(new Date(isoString), 'dd/MM/yyyy')
+  }
 
   return (
     <div>
@@ -98,7 +136,9 @@ const DepartmentsPage = () => {
         <div className="departments">
           <div className="departments-titles">
             <div className="first-col">
-              <h3>Departments</h3>
+              <h3>
+                Departments
+              </h3>
               <p>Available departments</p>
             </div>
             <div className="second-col">
@@ -106,6 +146,25 @@ const DepartmentsPage = () => {
                 <PlusIcon />
                 Add department
               </button>
+              <div className="sort-controls">
+                <div>
+                  <ListFilter
+                    size={24}
+                    onClick={() => handleSortChange(sortConfig.field)}
+                    className="sort-btn"
+                  >
+                    {sortConfig.direction === 'asc' ? '↑' : '↓'}
+                  </ListFilter>
+                </div>
+                <select
+                  value={sortConfig.field}
+                  onChange={(e) => handleSortChange(e.target.value)}
+                  className="sort-select"
+                >
+                  <option>Sort By: Name</option>
+                  <option>Sort By: Date Added</option>
+                </select>
+              </div>
             </div>
           </div>
           {showAddDepartment && (
@@ -116,25 +175,24 @@ const DepartmentsPage = () => {
             />
           )}
           <div className="departments-list">
-            {Array.isArray(departments) && departments.length > 0
-              ? departments.map((department) => (
-                  <div
-                    key={department.id}
-                    className="department-item"
-                    onClick={() => handleDepartmentClick(department.id)}
-                  >
-                    <Notebook size={30} className="department-icon" />
-                    <div>
-                      <h3 className="department-title">{department.name}</h3>
-                      <p>
-                        Members:{" "}
-                        {Array.isArray(department.members)
-                          ? department.members.length
-                          : department.members}
-                      </p>
-                    </div>
+            {sortedDepartments.length > 0
+              ? sortedDepartments.map((department) => (
+                <div
+                  key={department.id}
+                  className="department-item"
+                  onClick={() => handleDepartmentClick(department.id)}
+                >
+                  <Notebook size={30} className="department-icon" />
+                  <div>
+                    <h3 className="department-title">{department.name}</h3>
+                    <p
+                      className="date"
+                    >
+                      Created on {formatDate(department.created_at)}
+                    </p>
                   </div>
-                ))
+                </div>
+              ))
               : "No departments found"}
           </div>
         </div>
