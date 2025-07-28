ca677545d3476b396a22e2f1931d69916d9e281d
Aphonse <csrlimitedrwanda@gmail.com>
2025-07-28 10:43:14 +0200

    added new pages

---
+++ src/app/incident/general/[incidentId]/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+import DashboardLayout from '@/app/dashboard/layout'
+import React from 'react'
+
+const page = () => {
+    return (
+        <DashboardLayout>
+
+        </DashboardLayout>
+    )
+}
+
+export default page
