91658d731fa10f791f01a174660dfc3e01fc1a1d
Charmant12 <sgcharmant@compstaffing.com>
2025-07-31 11:34:27 +0200

    Added medication error report details

---
+++ src/app/incident/medication-error/[incidentId]/page.jsx
@@ -0,0 +1,11 @@
+import MedicationDetails from "@/components/incidents/incidentDetails/medicationError/MedicationDetails"
+
+const page = () => {
+  return (
+    <>
+      <MedicationDetails />
+    </>
+  )
+}
+
+export default page
