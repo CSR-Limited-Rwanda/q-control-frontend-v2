14b1bb689486ecb212b1f75d3556da4a696f1562
Aphonse <csrlimitedrwanda@gmail.com>
2025-08-19 15:40:07 +0200

    added new files

---
+++ src/components/incidents/incidentForms/LostAndFoundForms/steps/Step1Info.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Step1Info = () => {
+    return (
+        <div>Step1Info</div>
+    )
+}
+
+export default Step1Info
---
+++ src/components/incidents/incidentForms/LostAndFoundForms/steps/Step2Actions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Step2Actions = () => {
+    return (
+        <div>Step2Actions</div>
+    )
+}
+
+export default Step2Actions
